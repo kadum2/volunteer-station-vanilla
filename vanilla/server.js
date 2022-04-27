f88b0a4ad056347477c94f4fba17e6a190931bb1
kadum <75564464+kadum2@users.noreply.github.com>
2022-04-27 16:31:18 +0300

    f1; account making; register; ui, dataflow, server, db; check if em exist to add or not

--- vanilla/server.js
+++ vanilla/server.js
@@ -1,31 +1,63 @@
 
+const { json } = require("express");
 const express = require("express")
 const app = express()
 const path = require("path");
 const { env } = require("process");
+require("dotenv").config()
 
+/////config
+app.use(express.json())
 // require("ejs")
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+let mongodb = require("mongodb").MongoClient  ///mongodb atlas
 
+////sending pages; 
 ///setting public dirs
 app.use(express.static("public"))
 // app.use("/home",express.static("./home"))
 // app.use("/profile",express.static("./profile"))
 
-
-///home route
+///home page route
 let container = [1, 2, 3]
 app.get("/home", (req, res)=>{
     res.render("home.ejs", {container})
 })
-
-
-///profile route
+///profile page route
 app.get("/profile", (req, res)=>{
     res.render("profile.ejs")
 })
+
+
+
+
+
+////registering routes
+app.post("/regUser", (req, res)=>{
+    console.log(".....post regUser........")
+    console.log(req.body)
+
+    ////call mongodb
+    mongodb.connect(process.env.MONGOKEY, async (err, client)=>{
+        let dbb = client.db()
+
+        let dbusers = await dbb.collection("users").find({}).toArray()
+        console.log(dbusers)
+
+        if(await dbb.collection("users").findOne({em: req.body.em})){
+            console.log("exist")
+        }else{
+            console.log("doesnt exist")
+            dbb.collection("users").insertOne(req.body)
+        }
+        // dbb.collection("users").insertOne(req.body)
+        })
+})
+
+
+
+
 
 
 
