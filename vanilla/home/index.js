8f26910eb70577f684cb9ac11e6a69fe6ce80159
kadum <75564464+kadum2@users.noreply.github.com>
2022-05-11 13:55:45 +0300

    home; get posts; main filters; frontend

--- vanilla/home/index.js
+++ vanilla/home/index.js
@@ -1,3 +1,4 @@
+
         
         ////// dom elements
         let auth = document.querySelector("#auth")
@@ -19,12 +20,59 @@
 
         let currentUser
         let token
+        let postsArray
+        let filtered = []
+
+        // .prototype.forEach = Array.prototype.forEach
+
+        /////the filters 
+        let filters = document.querySelector("#main-filters")
+        console.log(filters.children)
+        for(let item of filters.children){
+            console.log(item)
+            
+                item.addEventListener("change", (e)=>{
+                    console.log("changed filter value ")
+                    console.log(e.target.value)
+                    // postsArray.forEach(i=>
+                    // filtered.push(postsArray.filter(i[item.getAttribute("id")]==
+                    // e.target.value)))
+                    filtered = postsArray.filter(i=> i[item.getAttribute("id")] == e.target.value)
+                    console.log(filtered)
+                    makingPosts(filtered)
+                })
+            
+        }
+        // [].forEach.call(filters.children, )
+        // filters.children.forEach(e=>{
+        //     e.addEventListener("change", ()=>{
+        //         makingPosts(postsArray.forEach(i=> postsArray.filter(i[e.getAttribute("id")]== e.target.value)))
+        //     })
+        // })
+
+        let timeState = document.querySelector("#timeState")
+        let campLocation = document.querySelector("#location")
+        let campType = document.querySelector("#campType")
+        let campPrototype= document.querySelector("#campPrototype")
+        let skills = document.querySelector("#skills")
+        let knowledge = document.querySelector("#knowledge")
+        let toolsMaterials = document.querySelector("#toolsMaterials")
+
+
+        // timeState.addEventListener("change", (e)=>{
+        //     console.log(e.target.value)
+        //     console.log(postsArray)
+        //     console.log(postsArray.filter(i=>i[`${timeState.getAttribute("id")}`] == e.target.value))
+        // })
 
 
 
         ///////
         function makingPosts(postsArray){
+
             console.log(postsArray)
+            document.querySelector("#cont-cont").innerHTML = ""
+            
             postsArray.forEach(e => {
                 console.log(e)
             let postTemplate = `<div class="post">
@@ -41,7 +89,7 @@
             <!-- second sectino -->
             <div class="currentState flex">
                 <div class="current-state-imgs flex">
-                ${e.cStateImgs.map(e=>  `<img style="background:url('../posts/${e}').;        
+                ${e.cStateImgs.map(e=>  `<img style="background:url('../posts/${e}');        
                 background-size: cover;
                 background-position: center center;
         ">`).join('')}
@@ -74,9 +122,9 @@
 
             <!-- fourth section -->
             <div class="requirements flex">
-                <p>${e.skills}; x/x</p>
-                <p>${e.knowledge}; x/x</p>
-                <p>${e.toolsMaterials}; x/x</p>
+                <span>${e.skills}; x/x</span>
+                <span>${e.knowledge}; x/x</span>
+                <span>${e.toolsMaterials}; x/x</span>
                 <h3>المبلغ المطلوب; ${e.neededDonation} </h3>
                 <h3>المبلغ الحالي; ${e.currentDonation} </h3>
             </div>
@@ -100,9 +148,10 @@
             let pd = await d.json()
 
             console.log(pd)
+            postsArray = pd.posts
 
             ////making posts 
-            makingPosts(pd.posts)
+            makingPosts(postsArray)
         }
 
 
